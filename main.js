ceb004b85e04407fe81e97446bdea4435175bcad
Lucag502 <84347577+Lucag502@users.noreply.github.com>
2023-08-16 16:26:42 -0300

    arreglando errores del addeventlistener

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const button1 = document.querySelector('#button1');
+window.onload = function(){
+
 const modal = document.querySelector('.modal');
+const button1 = document.querySelector('#button1');
+
 
 button1.addEventListener('click', modalEvent);
 
 function modalEvent (){
+    console.log('esexo anal');
     modal.classList.remove('hidden');
     modal.classList.add('visible');
-}
+}};
