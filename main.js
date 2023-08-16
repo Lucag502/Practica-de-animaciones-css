c0a86647eb3791d4daf08c8cadf4e30b78819280
Lucag502 <84347577+Lucag502@users.noreply.github.com>
2023-08-16 16:51:03 -0300

    el modal y sus eventos

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ window.onload = function(){
 
 const modal = document.querySelector('.modal');
 const button1 = document.querySelector('#button1');
+const closeButton = document.querySelector('.modal-content-close');
 
 
 button1.addEventListener('click', modalEvent);
+closeButton.addEventListener('click', closeEvent);
 
 function modalEvent (){
-    console.log('esexo anal');
     modal.classList.remove('hidden');
     modal.classList.add('visible');
+};
+
+function closeEvent (){
+    modal.classList.add('hidden');
+    modal.classList.remove('visible');
 }};
