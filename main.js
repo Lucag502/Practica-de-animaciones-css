bc7942acb8a8ca79db160f5e184dae62faa6fa97
Lucag502 <84347577+Lucag502@users.noreply.github.com>
2023-08-16 16:02:06 -0300

    agregando mas bordes y detalles

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 const button1 = document.querySelector('#button1');
 const modal = document.querySelector('.modal');
+
+button1.addEventListener('click', modalEvent);
+
+function modalEvent (){
+    modal.classList.remove('hidden');
+    modal.classList.add('visible');
+}
